1850cc49d618db9b5bd53d8c911e5bb13fa1a3d9
Alex Alexa <alex.saranciuc@gmail.com>
2023-12-30 18:25:51 +0000

    Optimization

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { urlExelTable } from "../constants";
 const Footer = () => {
 	return (
 		<footer className={classes.tablelink}>
-			<img src="./public/img/svg/excel-logo.svg" alt="excel logo" />
+			<img src="./img/svg/excel.svg" alt="excel logo" />
 			<a href={urlExelTable} target="_blank" rel="full statistics on google excel">
 				The table can also be found at this link.
 			</a>
